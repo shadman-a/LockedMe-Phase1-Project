bb41232d7b6f972f1728686df3fcf092594bc938
shadman-a <shadman@outlook.com>
2021-01-13 15:35:43 -0500

    added show files/add file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
         mainMenu();
     }
+
+    final static String FOLDER = "/tmp";
 
     public static void mainMenu() {
         Scanner scanner = new Scanner(System.in);
@@ -14,7 +23,7 @@ public class Main {
 
         switch (mainMenuSelection) {
             case "1":
-                readFiles();
+                showFiles();
                 break;
             case "2":
                 operationsMenu();
@@ -55,12 +64,48 @@ public class Main {
 
     }
 
-    public static void readFiles()  {
-        System.out.println("readFiles");
+    public static void showFiles()  {
+        System.out.println("Showing files in ascending order:");
+
+        File[] files = new File(FOLDER).listFiles();
+
+        Set<String> sorted = new TreeSet<>();
+
+        for (File file: files) {
+            sorted.add(file.getName());
+        }
+        sorted.forEach(System.out::println);
+        System.out.println("------------------");
+        mainMenu();
     }
 
     public static void addFile() {
-        System.out.println("add");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please provide a file path");
+        String filePath = scanner.nextLine();
+        Path path = Paths.get(filePath);
+
+        if (!Files.exists(path)) {
+            System.out.println("File does not exist");
+        }
+
+
+
+        String newFilePath = FOLDER + "/" + path.getFileName();
+        int inc = 0;
+        while (Files.exists(Paths.get(newFilePath))) {
+            inc++;
+            newFilePath = FOLDER + "/" + inc + "_" + path.getFileName();
+        } try {
+            Files.copy(path, Paths.get(newFilePath));
+        } catch(IOException e) {
+            System.out.println("Unable to copy file to " + newFilePath);
+        }
+
+        System.out.println("newFilePath: " + newFilePath);
+        System.out.println("path" + path.toString());
+        System.out.println("------------------");
+        mainMenu();
     }
 
     public static void deleteFile() {
